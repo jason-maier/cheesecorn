d2532a87c6e9526fc36603f00c7a1ff0570f770a
jason-maier <jason.maier@fastly.com>
2020-01-15 14:01:08 -0800

    Removes unused content

--- app/routes/learning/ember.js
+++ app/routes/learning/ember.js
@@ -3,7 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return [
-      { name: 'Templates' },
+      {
+        name: 'Templates',
+        description: 'The building blocks of Ember. Spoiler alert: it ends up being a bunch of HTML at the end.',
+      },
       { name: 'Components' },
     ];
   }
